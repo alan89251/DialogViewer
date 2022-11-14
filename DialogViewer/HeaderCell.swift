e5e78d56bf941ade2fca968ac8c5da95df86127c
Alan <csuen2@my.centennialcollege.ca>
2022-11-13 22:54:26 -0500

    Implement the Dialog Viewer

---
+++ DialogViewer/HeaderCell.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderCell.swift
+//  DialogViewer
+//
+//  Created by bee on 13/11/2022.
+//
+
+import UIKit
+
+class HeaderCell: ContentCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.backgroundColor = UIColor(red: 0.9, green: 0.9,
+                                        blue: 0.8, alpha: 1.0)
+        label.textColor = UIColor.black
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override class func defaultFont() -> UIFont {
+        return UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+    }
+}
